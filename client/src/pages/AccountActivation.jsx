48c0c8f8bee4237b016c1a89491de2ef058c0c4a
Miyad Youssouf <youssoufmiyad@gmail.com>
2024-07-14 17:57:12 +0200

    petit message de confirmation de création du compte

--- client/src/pages/AccountActivation.jsx
+++ client/src/pages/AccountActivation.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Stack, Typography } from "@mui/material";
 
 const AccountActivation = () => {
-  return (
-    <div>
-      account created
-    </div>
-  )
-}
+	return (
+		<div style={{display:"flex", justifyContent:"center", textAlign:"center"}}>
+			<Stack
+				sx={{ backgroundColor: "#82B2CB", width: "100%", margin:"18rem"}}
+			>
+        <Typography
+					fontWeight="900"
+					sx={{ fontSize: "70px", opacity: 0.6, fontFamily: "Spartan" }}
+          mt={6}
+          mb={12}
+				>
+					account successfully created ! <br />
+				</Typography>
 
-export default AccountActivation
+        <Typography
+				fontStyle="italic"
+				fontSize="22px"
+				lineHeight="35px"
+				mb={6}
+			>
+				You can now sign in and use every features of the app. <span style={{fontWeight:"700"}}>ENJOY !</span>
+			</Typography>
+
+				
+			</Stack>
+		</div>
+	);
+};
+
+export default AccountActivation;
